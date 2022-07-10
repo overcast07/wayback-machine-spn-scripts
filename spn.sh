d4f9b5f7d33535020a96c5207743ed9c630e4b80
overcast <60053886+overcast07@users.noreply.github.com>
2022-07-10 22:13:04 +0800

    Update spn.sh
    Error message was changed to "You have already reached the limit of active Save Page Now sessions. Please wait for a few minutes and then try again.", which broke the matching function

--- spn.sh
+++ spn.sh
@@ -265,7 +265,7 @@ function capture(){
 				fi
 			else
 				echo "$message"
-				if ! [[ "$message" =~ "You have already reached the limit of active sessions" || "$message" =~ "Cannot start capture" || "$message" =~ "The server encountered an internal error and was unable to complete your request" || "$message" =~ "Crawling this host is paused" ]]; then
+				if ! [[ "$message" =~ "You have already reached the limit" || "$message" =~ "Cannot start capture" || "$message" =~ "The server encountered an internal error and was unable to complete your request" || "$message" =~ "Crawling this host is paused" ]]; then
 					if [[ "$message" =~ "You cannot make more than "[1-9][0-9,]*" captures per day" ]]; then
 						touch daily_limit$f.txt
 						break 2
@@ -311,7 +311,7 @@ function capture(){
 							fi
 						else
 							echo "$message"
-							if [[ "$message" =~ "You have already reached the limit of active sessions" || "$message" =~ "Cannot start capture" || "$message" =~ "The server encountered an internal error and was unable to complete your request" || "$message" =~ "Crawling this host is paused" ]]; then
+							if [[ "$message" =~ "You have already reached the limit" || "$message" =~ "Cannot start capture" || "$message" =~ "The server encountered an internal error and was unable to complete your request" || "$message" =~ "Crawling this host is paused" ]]; then
 								:
 							elif [[ "$message" =~ "You cannot make more than "[1-9][0-9,]*" captures per day" ]]; then
 								rm lock$f.txt
